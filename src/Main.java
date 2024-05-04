14b32189c4cccb288c2506db63d4ac50cfb303d6
mohammedqasim91 <mohamadqasim888@gmail.com>
2024-05-05 00:56:04 +0200

    Standard Input/Output

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Read from standard input a sentence,
+        //Then prints the count of the words that starts with a vowel litters,
+        //Each word that does not start with a vowel should be skipped and an error message.
+
+        Scanner vowel = new Scanner(System.in);
+        String line = vowel.nextLine();
+        int vowelCount = 0;
+        String[] vowelWords = line.split(" ");
+        System.out.println("Please inter a phrase to be counted");
+
+        for (String word : vowelWords) {
+            if (word.startsWith("a")  ||
+                word.startsWith("e")  ||
+                word.startsWith("i")  ||
+                word.startsWith("o")  ||
+                word.startsWith("u") )
+                {
+                    vowelCount++;
+                }
+            else {
+                System.out.println("Not a vowel" + word);
+                }
+        }
+
+        System.out.println("vowelWords: " + vowelCount);
+
+        vowel.close();
+
+    }
+}
